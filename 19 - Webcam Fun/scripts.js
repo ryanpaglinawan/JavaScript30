075c1efa9a833642f489eba7a809a0342d3b69ef
Ryan Paglinawan <ryanpaglinawan@Ryan-Paglinawan-MBP16.local>
2021-11-19 12:37:36 +0800

    add js script for 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,38 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+const videoStream = () => {
+  navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+    video.srcObject = stream;
+    video.play();
+  });
+};
+
+const showCanvas = () => {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 10);
+};
+
+const takePhoto = () => {
+  const link = document.createElement("a");
+  const data = canvas.toDataURL("image/jpeg");
+  let ctr = strip.children.length + 1;
+  link.href = data;
+  link.setAttribute("download", `Photo-${ctr}`);
+  link.innerHTML = `<img src="${data}" alt="Photo-${ctr}" />`;
+  strip.insertBefore(link, strip.firstChild);
+  snap.currentTime = 0;
+  snap.play();
+};
+
+videoStream();
+video.addEventListener("canplay", showCanvas);
